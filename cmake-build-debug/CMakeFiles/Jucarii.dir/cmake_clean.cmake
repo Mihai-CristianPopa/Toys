aeb47596fc70186c5f57f491c456490573634be7
Mihai-Cristian Popa <mihaipopa00@gmail.com>
2022-04-26 17:31:18 +0300

    added all the files

---
+++ cmake-build-debug/CMakeFiles/Jucarii.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Jucarii.dir/main.cpp.obj"
+  "Jucarii.exe"
+  "Jucarii.exe.manifest"
+  "Jucarii.pdb"
+  "libJucarii.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Jucarii.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
